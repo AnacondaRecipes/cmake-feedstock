c59d311d7bb11a66abf267cdc8823adef822c88c
Ray Donnelly <mingw.android@gmail.com>
2018-05-09 13:05:04 +0100

    Temporary commit, we cannot build CMake atm due to:
    CMake Error at CMakeLists.txt:92 (message):
      The C++ compiler does not support C++11 (e.g.  std::unique_ptr).
    
    this may just be for CTest, source is:
    
    https://gitlab.kitware.com/cmake/cmake/blob/master/CMakeLists.txt#L91-93
    
    Going to repackage the binary instead as we need a new cmake ASAP

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,22 +1,19 @@
 
-if "%ARCH%"=="32" (set CPU_ARCH=x86) else (set CPU_ARCH=x64)
-curl https://cmake.org/files/v%PKG_VERSION:~0,4%/cmake-%PKG_VERSION%-win%ARCH%-%CPU_ARCH%.zip -o cmake-win.zip
-7z x cmake-win.zip > nil
-set PATH=%CD%\cmake-%PKG_VERSION%-win%ARCH%-%CPU_ARCH%\bin;%PATH%
-cmake --version
+set PATH=%CD%\official-binary\bin;%PATH%
 
-mkdir build && cd build
+mkdir build
+pushd build
 
-set CMAKE_CONFIG="Release"
+  set CMAKE_CONFIG="Release"
 
-dir /p %LIBRARY_PREFIX%\lib
+  dir /p %LIBRARY_PREFIX%\lib
 
-cmake -LAH -G"NMake Makefiles"                               ^
-    -DCMAKE_BUILD_TYPE=%CMAKE_CONFIG%                        ^
-    -DCMAKE_FIND_ROOT_PATH="%LIBRARY_PREFIX%"                ^
-    -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ..
-if errorlevel 1 exit 1
+  cmake -LAH -G"NMake Makefiles"                               ^
+      -DCMAKE_BUILD_TYPE=%CMAKE_CONFIG%                        ^
+      -DCMAKE_FIND_ROOT_PATH="%LIBRARY_PREFIX%"                ^
+      -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ..
+  if errorlevel 1 exit 1
 
-cmake --build . --config %CMAKE_CONFIG% --target install
-if errorlevel 1 exit 1
-
+  cmake --build . --config %CMAKE_CONFIG% --target install
+  if errorlevel 1 exit 1
+popd
