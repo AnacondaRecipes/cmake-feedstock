54bcf3dc0e7a79ed0e9ba998ef742827f4904afd
Ray Donnelly <mingw.android@gmail.com>
2018-05-09 12:46:17 +0000

    Linux fixes

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,9 +1,20 @@
 #!/bin/sh
 
+# Filter out -I${PREFIX}/include from CXXFLAGS as it causes issues with bad header
+# *and* macro name hygiene around libuv (queue.h / QUEUE etc, urgh).
+re="(.*[[:space:]])\-I${PREFIX}/include[^[:space:]]*(.*)"
+if [[ "${CXXFLAGS}" =~ $re ]]; then
+  export CXXFLAGS="${BASH_REMATCH[1]}${BASH_REMATCH[2]}"
+fi
+if [[ "${CFLAGS}" =~ $re ]]; then
+  export CFLAGS="${BASH_REMATCH[1]}${BASH_REMATCH[2]}"
+fi
+
 ./bootstrap \
              --prefix="${PREFIX}" \
              --system-libs \
              --no-qt-gui \
+             --no-system-libuv \
              --no-system-libarchive \
              --no-system-jsoncpp \
              --parallel=${CPU_COUNT} \
