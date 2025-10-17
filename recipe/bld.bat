a7c3154372ead5242fb3aa2598f991398645feb7
Andrii Osipov <aosipov@anaconda.com>
2025-10-17 02:51:36 -0700

    try without symbolic linl

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -12,15 +12,6 @@ if "%PY_INTERP_DEBUG%" neq "" (
   set CMAKE_CONFIG="Release"
 )
 
-REM latest xz version 5.6.4 failed on windows with:
-REM missing: LIBLZMA_HAS_AUTO_DECODER LIBLZMA_HAS_EASY_ENCODER LIBLZMA_HAS_LZMA_PRESET
-REM The latest xz 5.6.4 for win-64 doesn`t consist of liblzma.lib, now it's called lzma.lib
-if exist "%LIBRARY_PREFIX%\lib\lzma.lib" (
-  if not exist "%LIBRARY_PREFIX%\lib\liblzma.lib" (
-    mklink "%LIBRARY_PREFIX%\lib\liblzma.lib" "%LIBRARY_PREFIX%\lib\lzma.lib"
-  )
-)
-
 dir /p %LIBRARY_PREFIX%\lib
 
 cmake -LAH -G Ninja                                          ^
@@ -36,7 +27,7 @@ cmake -LAH -G Ninja                                          ^
     -DCMAKE_USE_SYSTEM_BZIP2=TRUE                            ^
     -DZLIB_LIBRARY="%LIBRARY_LIB%\zlib.lib"                  ^
     -DZLIB_INCLUDE_DIR="%LIBRARY_INC%"                       ^
-    -DLIBLZMA_LIBRARY:FILEPATH="%LIBRARY_LIB%\liblzma.lib"   ^
+    -DLIBLZMA_LIBRARY:FILEPATH="%LIBRARY_LIB%\lzma.lib"      ^
     -DBZIP2_INCLUDE_DIR="%LIBRARY_INC%"                      ^
     -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ..
 if errorlevel 1 exit 1
